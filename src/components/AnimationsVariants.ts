9eb9f59a2d934230637357f0c2486001c1d40d34
Omar Hernández <127421759+omar15hr@users.noreply.github.com>
2025-05-15 09:53:41 -0400

    arreglamos el hot reload

---
+++ src/components/AnimationsVariants.ts
@@ -0,0 +1,70 @@
+import { Variants } from "framer-motion";
+
+export const fadeIn = (
+  direction: "up" | "down" | "left" | "right",
+  delay: number = 0
+): Variants => {
+  return {
+    hidden: {
+      y: direction === "up" ? 40 : direction === "down" ? -40 : 0,
+      x: direction === "left" ? -40 : direction === "right" ? -40 : 0,
+      opacity: 0,
+    },
+    show: {
+      y: 0,
+      x: 0,
+      opacity: 1,
+      transition: {
+        type: "tween",
+        duration: 0.8,
+        delay,
+        ease: [0.25, 0.25, 0.25, 0.75],
+      },
+    },
+  };
+};
+
+export const zoomIn = (delay: number = 0, scale: number = 0.85): Variants => {
+  return {
+    hidden: {
+      scale,
+      opacity: 0,
+    },
+    show: {
+      scale: 1,
+      opacity: 1,
+      transition: {
+        type: "tween",
+        delay,
+        duration: 0.8,
+        ease: [0.25, 0.25, 0.25, 0.75],
+      },
+    },
+  };
+};
+
+export const slideIn = (
+  direction: "up" | "down" | "left" | "right",
+  type: string,
+  delay: number,
+  duration: number
+): Variants => {
+  return {
+    hidden: {
+      x: direction === "left" ? "-100%" : direction === "right" ? "100%" : 0,
+      y: direction === "up" ? "100%" : direction === "down" ? "-100%" : 0,
+      opacity: 0,
+    },
+    show: {
+      x: 0,
+      y: 0,
+      opacity: 1,
+      transition: {
+        type,
+        delay,
+        duration,
+        ease: "easeOut",
+      },
+    },
+  };
+};
